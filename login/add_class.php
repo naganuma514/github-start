7ec23a8bc89e742759c2abc7ff053ead4b114d8b
naganuma514 <naganuma514@icloud.com>
2020-10-07 22:21:57 +0900

    ログインシステム　メール正規表現まで

--- login/add_class.php
+++ login/add_class.php
@@ -2,18 +2,21 @@
 //ログインクラス
 class Login
 {
-    //メアド、パスワード、確認パスワードプロパティ
+    //メアド、パスワード、確認パスワードプロパティ。
     private $mail;
     private $password;
     private $password_conf;
-
-    public function __construct()
+    
+    //POSTがあったとき値を各プロパティにセット。
+    public function firstLogin()
     {
         if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST') {
             $this->mail = filter_input(INPUT_POST, 'mail');
             $this->password = filter_input(INPUT_POST, 'password');
             $this->password_conf = filter_input(INPUT_POST, 'password_conf');
         }
+    
+        //各ゲッター    
     }
     public function getMail()
     {
@@ -27,14 +30,45 @@ class Login
     {
         return $this->password_conf;
     }
-    //値がPOSTされていれば、フィルターを適用
+    
+    //値がPOSTされていれば、フィルターを適用し、プロパティに代入。
     public function checkInput($text,$err_text,$value) {
         if ($text === '') {
             $err['$err_text'] = "${value}は入力必須です。";
-            echo $err['$err_text'];
             return $err['$err_text'];
+        } 
+    }
+   
+    //確認用パスワードは同じ値を求める。
+    public function samePass($a,$b) {
+        if ($a !== $b) {
+            $err['password_conf'] = 'パスワードが一致しません。';
+            return $err['password_conf'];
         }
-
+    } 
+   
+    //エラー時エラー内容を表示。
+    public function errCheck($err) {
+        if(isset($err)) {
+            foreach($err as $er) {
+                echo $er;
+            }
+        }
+    }
+    
+    //メアドの正規表現。誤っていればエラーが返される。
+    public function mailCheck($mail) {
+        $reg_str = "/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/";
+        $e = "メールアドレスの入力に誤りがあります";
+        if (preg_match($reg_str, $mail)) {
+    //正規表現がTRUEなら何もしない。
+        } else if(empty($mail)) {
+    //空欄なら何もしない。
+        } else {
+            if(isset($mail)) {
+                return $e;  
+            }//ユーザーから入力があり、かつ正規表現から外れた場合にエラー表示。
+        }
     }
 }
 
--- login/adduser.php
+++ login/adduser.php
@@ -2,23 +2,39 @@
 
 ini_set('display_errors', true);
 error_reporting(E_ALL);
-//セッションスタート
+
+//セッションスタート。
 session_start();
-//DBとClassの読み込み
+
+//DBとClassの読み込み。
 require dirname(__FILE__) . '/database.php';
 require dirname(__FILE__) . '/add_class.php';
 
-//ログインインスタンス生成
+$err=[]; //エラーメッセージ格納配列。
+
+//ログインインスタンス生成。
 $user1=new Login();
-//各ゲッターを変数に代入
+$user1->firstLogin(); //新規登録用メソッド。
+
+//各ゲッターを変数に代入。
 $mail = $user1->getMail();
 $password = $user1->getPassword();
 $password_conf = $user1->getPassword_conf();
-$a=$user1->checkInput($mail,'mail','メールアドレス');
-$b=$user1->checkInput($password,'password','パスワード');
-if(isset($b)) {
-    echo $b;
-}
+
+//メアド、パスワードの入力チェック。未入力ならそれぞれエラーが代入される。
+$err[0] = $user1->checkInput($mail,'mail','メールアドレス');
+$err[1] = $user1->checkInput($password,'password','パスワード');
+
+//確認用パスワードが一致しているか調べ、異なればエラーが変数に代入される。
+$err[2] = $user1->samePass($password,$password_conf);
+
+//メアドの正規表現。誤っていればエラーが返される。
+$err[3] = $user1->mailCheck($mail);
+
+//エラーが飛ばされていれば、それを配列から取り出して表示。
+$user1->errCheck($err);
+    
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
