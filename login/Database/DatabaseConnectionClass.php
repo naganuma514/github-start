2ea32900bfc930792afd13a330a22f91f3b67589
taka <tomo05061@gmail.com>
2020-10-09 17:21:02 +0900

    fix: setting.phpの見直し

--- login/Database/DatabaseConnectionClass.php
+++ login/Database/DatabaseConnectionClass.php
@@ -1,17 +1,17 @@
 <?php
 class Connection {
-    private $main = 0;
+    private $setting_index = 0;
     private $connections = [];
 
-    function __construct($connections, int $main)
+    function __construct(array $connections, int $setting_index)
     {
         $this->connections = $connections;
-        if(count($connections) > $main && $main > -1) {
-            $this->$main = $main;
+        if(count($connections) > $setting_index && $setting_index > -1) {
+            $this->$setting_index = $setting_index;
         }
     }
 
     public function getConnection(): object {
-        return (object)$this->connections[$this->main];
+        return (object)$this->connections[$this->setting_index];
     }
 }
--- login/env/setting.php
+++ login/env/setting.php
@@ -2,25 +2,23 @@
 
 require './Database/DatabaseConnectionClass.php';
 
+const USE_SETTING_INDEX = 1;
+
+const SETTINGS = [
+    0 => [
+        'dsn' => 'mysql:host=mysql;dbname=login;charset=utf8mb4;',
+        'username' => 'root',
+        'password' => 'password',
+    ],
+    1 => [
+        'dsn' => 'mysql:host=localhost;dbname=login;charset=utf8mb4;',
+        'username' => 'root',
+        'password' => ''
+    ]
+];
+
 function getConnectionInfo()
 {
-
-    $main = 1;
-
-    $settings = [
-        0 => [
-            'dsn' => 'mysql:host=mysql;dbname=login;charset=utf8mb4;',
-            'username' => 'root',
-            'password' => 'password',
-        ],
-        1 => [
-            'dsn' => 'mysql:host=localhost;dbname=login;charset=utf8mb4;',
-            'username' => 'root',
-            'password' => ''
-        ]
-    ];
-
-    $connection = new Connection($settings, $main);
-
+    $connection = new Connection(SETTINGS, USE_SETTING_INDEX);
     return $connection->getConnection();
 }
